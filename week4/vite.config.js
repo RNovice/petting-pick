0476d5b6be935b0995f2124ab9331e3f6fae7f5e
Voke <google@voke.com>
2025-01-18 22:25:41 +0800

    fix: week4 config path typo

--- week4/vite.config.js
+++ week4/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/hex-24-react/':'/',
+  base: process.env.NODE_ENV === 'production' ? '/hex-2024-react/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
