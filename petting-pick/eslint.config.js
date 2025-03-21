3906abb0924673566f86e95ff3305f948b584d62
Voke <google@voke.com>
2025-03-21 23:01:45 +0800

    fix: lint err

--- petting-pick/eslint.config.js
+++ petting-pick/eslint.config.js
@@ -33,7 +33,7 @@ export default [
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
-        'warn',
+        'off',
         { allowConstantExport: true },
       ],
       'react/prop-types': 'off',
